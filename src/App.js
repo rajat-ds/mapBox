4a751009eb5693b32b4898ce6af2ebb171414581
rajat-ds <rajatkumarbind@gmail.com>
2020-08-11 23:59:00 +0530

    added map

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ReactMapGL from 'react-map-gl'
-function App() {
-  return (
-    <div className="App">
-     <ReactMapGL></ReactMapGL>
-    </div>
-  );
+class App extends Component{
+  state = {
+    viewport: {
+      width: "100vw",
+      height: "100vh",
+      latitude: 42.430472,
+      longitude: -123.334102,
+      zoom: 16
+    }
+  }
+  render(){
+    return (
+ 
+      <div className="App">
+     <ReactMapGL  {...this.state.viewport}  onViewportChange={(viewport) => this.setState({viewport})} mapboxApiAccessToken='pk.eyJ1IjoicmFqYXQta2FzaHlhcDgyOTkiLCJhIjoiY2tkcTlmMTJyMjlrNTJ6bXJ1OTBpYnM3dSJ9.xHhy0kOaSPGW7nctrh55Ng'></ReactMapGL>
+      </div>
+    );
+  }
+
 }
 
 export default App;
