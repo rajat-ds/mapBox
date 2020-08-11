2ab8f96335d38313661f40f66c83545bc3c923ae
rajat-ds <rajatkumarbind@gmail.com>
2020-08-12 00:27:20 +0530

    added geolocation control

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ReactMapGL from 'react-map-gl'
+import ReactMapGL ,{GeolocateControl }from 'react-map-gl'
+const TOKEN = 'pk.eyJ1IjoicmFqYXQta2FzaHlhcDgyOTkiLCJhIjoiY2tkcTlmMTJyMjlrNTJ6bXJ1OTBpYnM3dSJ9.xHhy0kOaSPGW7nctrh55Ng'
+const geolocateStyle = {
+  float: 'left',
+  margin: '50px',
+  padding: '10px'
+};
 class App extends Component{
   state = {
     viewport: {
-      width: "100vw",
-      height: "100vh",
-      latitude: 42.430472,
-      longitude: -123.334102,
-      zoom: 16
+      width: "100%",
+      height: 900,
+      latitude: 0,
+      longitude: 0,
+      zoom: 2
     }
   }
   render(){
     return (
  
       <div className="App">
-     <ReactMapGL  {...this.state.viewport}  onViewportChange={(viewport) => this.setState({viewport})} mapboxApiAccessToken='pk.eyJ1IjoicmFqYXQta2FzaHlhcDgyOTkiLCJhIjoiY2tkcTlmMTJyMjlrNTJ6bXJ1OTBpYnM3dSJ9.xHhy0kOaSPGW7nctrh55Ng'></ReactMapGL>
+     <ReactMapGL  {...this.state.viewport}  onViewportChange={(viewport) => this.setState({viewport})} mapboxApiAccessToken={TOKEN}>
+     <GeolocateControl
+          style={geolocateStyle}
+          positionOptions={{enableHighAccuracy: true}}
+          trackUserLocation={true}
+        />
+     </ReactMapGL>
       </div>
     );
   }
